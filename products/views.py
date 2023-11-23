6c26fbf28cf4bd296c9174034e99dfc3bbc89ec2
Konstantin Parakhin <prospero19902506@gmail.com>
2023-11-23 18:15:58 +0300

    commons

--- products/views.py
+++ products/views.py
@@ -4,23 +4,22 @@ from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
 from products.models import ProductCategory, Product, Basket
+from commons.views import TitleMixin
 
-
-
-class IndexView(TemplateView):
+class IndexView(TitleMixin, TemplateView):
 	template_name = 'products/index.html'
-	
-	def get_context_data(self, **kwargs):
-		context = super(IndexView, self).get_context_data()
-		context['title'] = 'Store'
-		return context
+	title = 'Store'
+	# def get_context_data(self, **kwargs):
+	# 	context = super(IndexView, self).get_context_data()
+	# 	context['title'] = 'Store'
+	# 	return context
 
 
-class ProductsListView(ListView):
+class ProductsListView(TitleMixin, ListView):
 	model = Product
 	template_name = 'products/products.html'
 	paginate_by = 3
-	
+	title = 'Store - Каталог'
 	def get_queryset(self):
 		queryset = super(ProductsListView, self).get_queryset()
 		category_id = self.kwargs.get('category_id')
@@ -28,7 +27,6 @@ class ProductsListView(ListView):
 	
 	def get_context_data(self, *, object_list=None, **kwargs):
 		context = super(ProductsListView, self).get_context_data()
-		context['title'] = 'Store - Каталог'
 		context['categories'] = ProductCategory.objects.all()
 		return context
 
